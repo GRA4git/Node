fa0986d96908cb0950e0dbfc3e6f026ec892de1e
Gra4git <Gra4git@gmail.com>
2021-10-07 17:31:58 +0300

    8 commit

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/',(req, res) => {
-    res.send('Hello world')  
+app.get('/', (req, res) => {
+    res.send('Hello world')
 })
-app.get('/users',(req, res) => {
-    res.send(["user1", "user2", "user3"])  
+app.get('/users', (req, res) => {
+    res.json(['user1", "user2", "user3'])
 })
-app.post('/users',(req, res) => {
-    res.send('юзер добавлен')  
+app.post('/users', (req, res) => {
+    res.send('юзер добавлен')
 })
-app.get('/admin',(req, res) => {
-    res.send('не хватает прав доступа')  
+app.delete('/users/:id', (req, res) => {
+    res.send('юзер с ID :id удален')
 })
-
-app.listen(port,() => {
-console.log('VVVVVVVV')
+app.patch('/users/:id', (req, res) => {
+    res.send('юзер с ID :id изменен')
 })
-
+app.get('/admin', (req, res) => {
+    res.send('не хватает прав доступа')
+})
+app.listen(port, () =>{
+    console.log('YYYYYY')
+})
