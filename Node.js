865b3cd50f595ab18c71d9cddeead49ebb0c0a95
Gra4git <Gra4git@gmail.com>
2021-10-07 13:03:28 +0300

    4 commit

--- Node.js
+++ Node.js
@@ -1,11 +1,17 @@
+const { log } = require('console');
 const express = require('express');
 const { start } = require('repl');
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) =>{
-    res.send('Hello');
-})
- app.listen(port, () =>{
-console.log( `has been ${port}`)
- })
+app.get('/contacts', (req, res) => {
+    res.json({
+      city: "Grozny",
+      street: "Trosheva",
+      building: 7
+    })
+  })
+
+  app.listen(port,() => {
+      console.log('hhhhh');
+  })
