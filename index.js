bac490bdd723e7130e1aeb24b964fab171473f49
Gra4git <Gra4git@gmail.com>
2021-10-07 16:39:48 +0300

    7 commit

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/',(req, res) => {
-    res.send('Yeeeee')
+    res.send('Hello world')  
 })
+app.get('/users',(req, res) => {
+    res.send(["user1", "user2", "user3"])  
+})
+app.post('/',(req, res) => {
+    res.send('юзер добавлен')  
+})
+app.post('/',(req, res) => {
+    res.send('не хватает прав доступа')  
+})
+
 app.listen(port,() => {
 console.log('VVVVVVVV')
 })
+
